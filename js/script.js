64ad6a9310f12c05e72eb4e1303c16b8b8ee39fb
DESKTOP-3P2HVHB\b13 <webnobenkyou@gmail.com>
2017-04-19 16:32:59 +0900

    HTMLとCSS追記

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function(){
-    
-})
+    .cr-container input:checked ~ .cr-bgimg div span{
+	animation: slideOut 0.6s ease-in-out;
+}
+@keyframes slideOut{
+	0%{
+		left: 0px;
+	}
+	100%{
+		left: 150px;
+	}
+};
+});
